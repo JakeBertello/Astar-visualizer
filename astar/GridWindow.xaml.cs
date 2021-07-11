08568fb6647210a5e90193b516f7071a3e1a07d5
JakeBertello <jake.bertello@gmail.com>
2021-07-10 20:53:46 -0500

    Initial Commit

---
+++ astar/GridWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace astar
+{
+    /// <summary>
+    /// Interaction logic for Window2.xaml
+    /// </summary>
+    public partial class GridWindow : Window
+    {
+        public GridWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DrawTiles(int width, int height)
+        {
+            Graphics g = this.CreateGraphics();
+            Pen pen = new Pen(Brushes.Black, 3);
+
+            Rectangle rect = new Rectangle();
+            
+        }
+    }
+}
